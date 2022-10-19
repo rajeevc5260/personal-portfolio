996911adb7b19cdd274b0090eb0aefc2c832f4e8
Rajeev C <81346240+rajeevc5260@users.noreply.github.com>
2022-10-19 11:24:34 +0530

    changes

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toLanding() {
+    document.getElementById('landing')?.scrollIntoView({behavior:"smooth"});
   }
-
+  toServices() {
+    document.getElementById('services')?.scrollIntoView({behavior:"smooth"});
+  }
+  toContact() {
+    document.getElementById('contact')?.scrollIntoView({behavior:"smooth"});
+  }
+  toTestimony() {
+    document.getElementById('testimony')?.scrollIntoView({behavior:"smooth"});
+  }
 }
--- src/app/Components/landing/landing.component.ts
+++ src/app/Components/landing/landing.component.ts
@@ -23,4 +23,5 @@ export class LandingComponent implements OnInit {
   downloadPdf(pdfUrl: string, pdfName: string) {
     FileSaver.saveAs(pdfUrl, pdfName);
   }
+
 }
